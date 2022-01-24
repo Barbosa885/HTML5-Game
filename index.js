5911b02fc0ef15b1fc16b4b2e4bb004dad4282a1
Gustavo Barbosa <gustavosports7@gmail.com>
2022-01-24 19:32:09 -0300

    feat: Add the background scroll and the platform loop

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ class Player {
 }
 
 class Platform { 
-    constructor() {
+    constructor({x, y}) {
         this.position = {
-            x: 400,
-            y: 500
+            x,
+            y
         }
         this.width = 200,
         this.height = 20
@@ -56,7 +56,7 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({x: 400, y: 500}), new Platform({x: 600, y: 400})]
 
 const keys = {
     right: {
@@ -71,21 +71,38 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platform.draw()
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 10
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -10
-    } else player.velocity.x = 0
-
+    } else {
+        player.velocity.x = 0
+        
+        if (keys.right.pressed) { 
+            platforms.forEach((platform) => {
+                platform.position.x -= 10
+            })
+            
+        } else if (keys.left.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x += 10
+            })
+            
+        }
+    }
+platforms.forEach((platform) => {
     if (player.position.y + player.height <= platform.position.y && 
         player.position.y + player.height + player.velocity.y >= platform.position.y && 
         player.position.x + player.width >= platform.position.x && 
         player.position.x <= platform.position.x + platform.width) {
         player.velocity.y = 0
-    } 
-}    
+        } 
+    })
+} 
 
 animate()
 
@@ -103,7 +120,7 @@ window.addEventListener('keydown', ({keyCode}) => {
             console.log('right')
             keys.right.pressed = true
             break
-        case 87: 
+        case 32: 
             console.log('up')
             player.velocity.y -= 20
             break
