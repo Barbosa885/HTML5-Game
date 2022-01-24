266b4889b683c642794680684d03aff223071463
Gustavo Barbosa <gustavosports7@gmail.com>
2022-01-24 18:07:34 -0300

    feat: Add the platform colision

--- index.js
+++ index.js
@@ -25,22 +25,39 @@ class Player {
 
     draw() {
         c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height,)
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
     update() {
+        this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        this.draw()
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
         else this.velocity.y = 0
     }
+}
 
+class Platform { 
+    constructor() {
+        this.position = {
+            x: 400,
+            y: 500
+        }
+        this.width = 200,
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
 const player = new Player()
+const platform = new Platform()
+
 const keys = {
     right: {
         pressed: false
@@ -54,12 +71,20 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed) {
-        player.velocity.x = 20
+        player.velocity.x = 10
     } else if (keys.left.pressed) {
-        player.velocity.x = -20
+        player.velocity.x = -10
     } else player.velocity.x = 0
+
+    if (player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y && 
+        player.position.x + player.width >= platform.position.x && 
+        player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+    } 
 }    
 
 animate()
@@ -83,8 +108,8 @@ window.addEventListener('keydown', ({keyCode}) => {
             player.velocity.y -= 20
             break
     }
-
 })
+
 window.addEventListener('keyup', ({keyCode}) => {
     // console.log(keyCode)
     switch(keyCode) { 
@@ -99,10 +124,5 @@ window.addEventListener('keyup', ({keyCode}) => {
             console.log('right')
             keys.right.pressed = false
             break
-        case 87: 
-            console.log('up')
-            player.velocity.y = 0
-            break
     }
-    
 })
